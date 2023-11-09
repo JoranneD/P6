7cb016159d2c3274f46fcf0de0f26aa65de9609f
Joranne <dolcie.joranne@gmail.com>
2023-11-09 14:02:56 +0100

    FIX bugs de openModal

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,56 +1,46 @@
 import { loadWorks } from './works.js';
+//import { loadEditInterface } from './edit.js';
+
+
+// Déclarez les variables en dehors des fonctions pour les rendre accessibles à toutes les fonctions
+const editBtn = document.getElementById('editBtn');
+const modal = document.getElementById('modalWindow');
+const closeBtn = document.getElementById('closeBtn');
 
 export function openModal() {
-    // Récupérez l'élément "editBtn" par son ID
-    const editBtn = document.getElementById('editBtn');
-  
-    // Récupérez la modal par son ID
-    const modal = document.getElementById('modalWindow');
-  
+  // Vérifiez si l'élément avec l'ID 'editBtn' est présent
+  if (editBtn) {
     // Ajoutez un écouteur d'événements au clic sur "editBtn"
     editBtn.addEventListener('click', function() {
-        // Rend la modal visible en supprimant la classe "hidden"
-        modal.classList.remove('hidden');
-
-        // Chargez la galerie lorsque la modal est ouverte
-        loadWorks();
-
-        // Récupérez la galerie
-        const gallery = document.querySelector('.gallery');
-
-        // Récupérez le paragraphe "editSign" par son ID
-        // const editContent = document.querySelector('#editSign p');
-
-        // Ajoutez la galerie à l'intérieur du paragraphe "editSign"
-        editContent.appendChild(gallery);
+    // Rend la modal visible
+    modal.style.display = 'flex';
     });
+  } 
+  // else {
+  //   console.error("L'élément avec l'ID 'editBtn' n'est pas présent dans le document.");
+  // }
+      
+  // Chargez la galerie lorsque la modal est ouverte
+  //loadWorks();
+
+  // Récupérez la galerie
+  //const gallery = document.querySelector('.gallery');
+
+  // Récupérez le paragraphe "editSign" par son ID
+  // const editContent = document.querySelector('#editSign p');
+
+  // Ajoutez la galerie à l'intérieur du paragraphe "editSign"
+  //editContent.appendChild(gallery);
+  
 }
   
 export function closeModal() {
-    // Récupérez la modal par son ID
-    const modal = document.getElementById('myModal');
-  
-    // Récupérez le bouton de fermeture par son ID
-    const closeModalButton = document.getElementById('closeModal');
-  
-    // Ajoutez un écouteur d'événements pour fermer la modal en cliquant sur le bouton de fermeture
-    closeModalButton.addEventListener('click', function() {
-      // Cachez la modal en ajoutant la classe "hidden"
-      modal.classList.add('hidden');
-    });
-  
-    // Ajoutez un écouteur d'événements pour empêcher la propagation du clic dans la modal
-    modal.addEventListener('click', function(event) {
-      event.stopPropagation();
-    });
-  
-    // Ajoutez un écouteur d'événements pour fermer la modal en cliquant n'importe où sur la page en dehors de la modal
-    document.addEventListener('click', function(event) {
-      if (event.target === modal) {
-        // Cachez la modal en ajoutant la classe "hidden"
-        modal.classList.add('hidden');
-      }
-    });
+  // Ajoutez un écouteur d'événements pour fermer la modal en cliquant en dehors de la modal
+  window.addEventListener('click', function(event) {
+    if (event.target === modal || event.target === closeBtn) {
+      modal.style.display = "none";
+    }
+  });
 }
 
 
@@ -116,3 +106,19 @@ export function closeModal() {
 //       }
 //     });
 //   }
+
+// Ajoutez un écouteur d'événements pour fermer la modal en cliquant sur le bouton de fermeture
+    //closeModalButton.addEventListener('click', function() {
+      // Cachez la modal en ajoutant la classe "hidden"
+      //modal.classList.add('hidden');
+    //});
+  
+
+  
+    // Ajoutez un écouteur d'événements pour fermer la modal en cliquant n'importe où sur la page en dehors de la modal
+    //document.addEventListener('click', function(event) {
+      //if (event.target === modal) {
+        // Cachez la modal en ajoutant la classe "hidden"
+        //modal.classList.add('hidden');
+      //}
+    //});
